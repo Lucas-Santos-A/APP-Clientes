11d8ed09744f935fb5f04e937246429414ab9460
Lucas-Santos-A <lucas.salmeida@hotmail.com>
2020-06-23 19:56:11 -0300

    Cadastrar novo usuário e tratamento de erros.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from './usuario';
+import { AuthService } from '../auth.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +13,13 @@ export class LoginComponent {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
+  errors: String[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
     ) { }
 
   onSubmit() {
@@ -24,10 +29,29 @@ export class LoginComponent {
   preparaCadastrar(event){
     event.preventDefault();
     this.cadastrando = true;
+    this.mensagemSucesso = null;
   }
 
   cancelaCadastro(){
     this.cadastrando = false;
+    this.errors = [];
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService.salvar(usuario)
+    .subscribe(response => {
+      this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login"
+      this.errors = [];
+      this.cadastrando = false;
+      this.username = '';
+      this.password = '';
+    }, errorResponse => {
+      this.errors = errorResponse.error.errors;
+      this.mensagemSucesso = null;
+    })
   }
 
 }
